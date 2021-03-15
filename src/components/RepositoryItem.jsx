2154d2f967adb999d5d85cb954f2f637c93a9647
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-15 15:51:16 -0300

    :zap: feat: repository components

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem(props) {
+  const { repository } = props;
+
+  return (
+    <li>
+      <strong>{repository?.name}</strong>
+      <p>{repository?.description}</p>
+      <a target="_blank"  href={repository?.link}>Acessar repositório</a>
+    </li>
+  );
+};
