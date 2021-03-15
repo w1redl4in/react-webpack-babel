bef1614698e9da34fd4a2f50106a5cbd5c0f3ace
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-15 15:51:39 -0300

    :construction_worker: feat: fast-refreshgit add .

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -16,18 +17,27 @@ module.exports = {
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public', 'index.html'),
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
-  ],
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /nodule_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel'),
+            ].filter(Boolean),
+          },
+        },
       },
       {
         test: /\.scss$/,
