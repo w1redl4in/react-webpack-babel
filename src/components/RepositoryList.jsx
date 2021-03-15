aead50b39bcd92817066a32a6752f91a9dd6b848
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-15 16:28:34 -0300

    :zap: feat: fetching github api repos

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,23 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem'
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos').then(response => response.json()).then(data => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-      </ul>
+        {repositories.map(repository => (
+          <RepositoryItem repository={repository} key={repository.name} />
+        ))}
+       </ul>
     </section>
   )
 }
